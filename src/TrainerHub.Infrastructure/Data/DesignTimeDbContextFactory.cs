49f004c6df210b40c7e661a6a656665eecdaf0c6
DESKTOP-FSJQOC8\Bilal <reg.bilal@gmail.com>
2026-04-15 11:29:55 +0100

    cleanup

---
+++ src/TrainerHub.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace TrainerHub.Infrastructure.Data;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..", "TrainerHub.API"))
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
